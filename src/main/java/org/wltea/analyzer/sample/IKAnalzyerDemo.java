af522d5a05df493e5d1fc9915594f35ad809d4e1
Will Wang <willwangv@gmail.com>
2014-12-11 11:51:28 +0800

    update dictionary

--- src/main/java/org/wltea/analyzer/sample/IKAnalzyerDemo.java
+++ src/main/java/org/wltea/analyzer/sample/IKAnalzyerDemo.java
@@ -50,7 +50,7 @@ public class IKAnalzyerDemo {
 		//获取Lucene的TokenStream对象
 	    TokenStream ts = null;
 		try {
-			ts = analyzer.tokenStream("myfield", new StringReader("这是一个中文TOEFL分词奥数的例子，你可以直接运行它！IKAnalyer can analysis english text too"));
+			ts = analyzer.tokenStream("myfield", new StringReader("物理老师数学数学老师"));
 			//获取词元位置属性
 		    OffsetAttribute  offset = ts.addAttribute(OffsetAttribute.class); 
 		    //获取词元文本属性
